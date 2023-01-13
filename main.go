f0e55c3b73bebc0c4b0adf8fe19a3bd009453242
MaxKut3 <macsimkutyshev@yandex.ru>
2023-01-13 11:37:57 +0300

    telegram updates

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 func main() {
-	
+
+	var token string = "5859735143:AAGFvrZyYDwF7RlOkrca4Ekfx8rPpJJQU9k"
+	var url = "https://api.telegram.org/bot" + token
+
+	for {
+		ans, err := getUpdate(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(ans)
+	}
+
+}
+
+func getUpdate(url string) ([]UpdateModel, error) {
+
+	update, err := http.Get(url + "/getUpdates")
+	if err != nil {
+		return nil, err
+	}
+
+	defer update.Body.Close()
+
+	date, err := io.ReadAll(update.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var res Request
+
+	if err := json.Unmarshal(date, &res); err != nil {
+		return nil, err
+	}
+	return res.Result, nil
 }
