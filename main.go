8f032a632caad2223cfc7e27cd50d6e864829b48
MaxKut3 <macsimkutyshev@yandex.ru>
2023-01-16 15:42:04 +0300

    Task2 version 1.0

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"regexp"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -11,7 +13,7 @@ func main() {
 
 	bot, err := tgbotapi.NewBotAPI("5859735143:AAGFvrZyYDwF7RlOkrca4Ekfx8rPpJJQU9k")
 	if err != nil {
-		log.Panic(fmt.Errorf("Authorization failed", err))
+		log.Panic(fmt.Errorf("authorization failed", err))
 		panic(err)
 	}
 
@@ -26,10 +28,44 @@ func main() {
 
 	for update := range updates {
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		res, parsError := stringParsing(update.Message.Text)
+		if parsError != nil {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Невалидная строка")
+			if _, sendError := bot.Send(msg); sendError != nil {
+				fmt.Errorf("Send message failed", sendError)
+			}
+			panic(parsError)
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("%s, сумма вашей покупки составила: %s, в следующей валюте: %s ", update.Message.From.UserName, res[0], res[1]))
+		//msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 		if _, sendError := bot.Send(msg); sendError != nil {
 			log.Panic(fmt.Errorf("Send message failed", sendError))
 			panic(sendError)
 		}
 	}
+}
+
+// Извлечение суммы, валюты, продукта из сообщения
+func stringParsing(text string) ([]string, error) {
+
+	msgList := strings.Split(text, " ")
+	re, _ := regexp.Compile("[a-z]")
+
+	//Проверка на валидность
+	if matched, matchedError := regexp.MatchString("[a-z]", msgList[len(msgList)-1]); matched != true {
+		return nil, fmt.Errorf("Невалидная строка", matchedError)
+	}
+
+	listInd := re.FindStringIndex(msgList[len(msgList)-1])
+	fmt.Println(listInd)
+
+	i := listInd[0]
+
+	res := make([]string, 2, 2)
+	res[0] = msgList[len(msgList)-1][:i] //сумма
+	res[1] = msgList[len(msgList)-1][i:] //валюта
+
+	return res, nil
+
 }
